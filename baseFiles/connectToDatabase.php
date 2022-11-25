e5e44ae6375274d77b16bf773c0c65b62d4af764
gyoriIstvanAdam <gyori.istvan.adam@gmail.com>
2022-11-25 10:44:01 +0100

    Update connectToDatabase.php

--- baseFiles/connectToDatabase.php
+++ baseFiles/connectToDatabase.php
@@ -1,15 +1,14 @@
 <?php
 	$conn = new mysqli("localhost","root","",);
 
-	// Kapcsolat check
+	
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	} 
 	
-	// Adatbázis választás
 	$conn->select_db("products");
 	
-	//Ellenőrzés
+	
 	$conn->query("SET NAMES 'UTF8'");
 	$conn->query("SET CHARACTER SET UTF8");
 ?>
